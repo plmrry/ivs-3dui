1fba30f6db69e8cb8e107f379750456cc0505e25
Paul Murray <plmrry@gmail.com>
2016-04-13 15:22:43 -0500

    v2 bare bones

---
+++ app/d3DomDriver.js
@@ -0,0 +1,69 @@
+import d3 from 'd3';
+import debug from 'debug';
+import { Observable as stream } from 'rx';
+
+import apply from './apply.js';
+
+export default function makeD3DomDriver(selector) {
+	return function d3DomDriver(state_reducer$) {
+		const dom_state$ = state_reducer$
+			.scan(apply, d3.select(selector))
+			.do(() => debug('dom')('update'))
+			
+		dom_state$.subscribe();
+			
+		return {
+			state$: dom_state$,
+			select: function(selector) {
+				let selection$ = dom_state$
+					.map(dom => dom.select(selector))
+					.filter(s => s.node() !== null)
+					.shareReplay(1);
+				return {
+					observable: function() {
+						return selection$;
+					},
+					events: makeEventsGetter(selection$),
+					d3dragHandler: makeDragHandler(selection$)
+				};
+			}
+		};
+	};
+}
+
+function makeEventsGetter(selection$) {
+	return function(type) {
+		return selection$.flatMap(observableFromD3Event(type));
+	};
+}
+
+function makeDragHandler(selection$) {
+	return function() {
+		const handler = d3.behavior.drag();
+		const dragHandler$ = selection$
+			.map(s => {
+				handler.call(s); 
+				return handler;
+			});
+		return {
+			events: function(type) {
+				return dragHandler$.flatMap(observableFromD3Event(type));
+			}
+		};
+	};
+}
+
+function observableFromD3Event(type) {
+	return function(selection) {
+		return stream
+			.create(observer => 
+				selection.on(type, function(d) {
+					observer.onNext({
+						datum: d,
+						node: this,
+						event: d3.event
+					});
+				})
+			);
+	};
+}
