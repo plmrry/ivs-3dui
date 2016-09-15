23538c4f7e69d1219e9ad7d7a783c3eb43c542b9
Paul Murray <paul.murray@nytimes.com>
2016-09-15 19:27:18 -0400

    nuke

--- app/utilities/log.js
+++
@@ -1,1 +0,0 @@
-export default function log(d) { console.log.apply(console, arguments); }
--- server.js
+++ server.js
@@ -5,71 +5,74 @@ var path = require('path');
 var babelify = require('express-babelify-middleware');
 var serveIndex = require('serve-index');
 var rollup = require('rollup-endpoint');
+const cheerio = require('cheerio');
 
 var app = express();
 
 app.set('port', 9877);
 
+
+
 //
 // Application
 //
 
-const external = [
-  'three/three.js',
-  '@cycle/core',
-  'rx',
-  'd3',
-  'debug',
-  'underscore'
-];
-app.use('/bundle.js', babelify(external));
-
-var appPath = path.resolve(__dirname, 'app', 'index.js');
-
-app.use('/app.js', babelify(appPath, { external: external }));
-
-app.use(express.static(appPath));
-
+// const external = [
+//   'three/three.js',
+//   '@cycle/core',
+//   'rx',
+//   'd3',
+//   'debug',
+//   'underscore'
+// ];
+// app.use('/bundle.js', babelify(external));
 //
-// Development
+// var appPath = path.resolve(__dirname, 'app', 'index.js');
 //
+// app.use('/app.js', babelify(appPath, { external: external }));
+//
+// app.use(express.static(appPath));
+//
+// //
+// // Development
+// //
+//
+// var devPath = path.resolve(__dirname, 'development');
+//
+// app.use('/development', serveIndex(devPath));
+// app.use('/development', express.static(devPath));
+//
+// const spawn = require('child_process').spawn;
+//
+// const nodeResolve = require('rollup-plugin-node-resolve');
+// const commonjs = require('rollup-plugin-commonjs');
 
-var devPath = path.resolve(__dirname, 'development');
-
-app.use('/development', serveIndex(devPath));
-app.use('/development', express.static(devPath));
-
-const spawn = require('child_process').spawn;
-
-const nodeResolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
-
-function glsl () {
-	return {
-		transform ( code, id ) {
-			if ( !/\.glsl$/.test( id ) ) return;
-
-			return 'export default ' + JSON.stringify(
-				code
-					.replace( /[ \t]*\/\/.*\n/g, '' )
-					.replace( /[ \t]*\/\*[\s\S]*?\*\//g, '' )
-					.replace( /\n{2,}/g, '\n' )
-			) + ';';
-		}
-	};
-}
-
-app.use('/ivs2.js', rollup.serve({
-  entry: path.resolve(__dirname, 'app2', 'index.js'),
-  plugins: [
-    nodeResolve(),
-    commonjs({
-      include: 'node_modules/**',
-			exclude: [ 'node_modules/three/**' ]
-    }),
-    glsl()
-  ]
-}));
+// function glsl () {
+// 	return {
+// 		transform ( code, id ) {
+// 			if ( !/\.glsl$/.test( id ) ) return;
+//
+// 			return 'export default ' + JSON.stringify(
+// 				code
+// 					.replace( /[ \t]*\/\/.*\n/g, '' )
+// 					.replace( /[ \t]*\/\*[\s\S]*?\*\//g, '' )
+// 					.replace( /\n{2,}/g, '\n' )
+// 			) + ';';
+// 		}
+// 	};
+// }
+//
+// app.use('/ivs2.js', rollup.serve({
+//   entry: path.resolve(__dirname, 'app2', 'index.js'),
+//   plugins: [
+//     nodeResolve(),
+//     commonjs({
+//       include: 'node_modules/**',
+// 			exclude: [ 'node_modules/three/**' ]
+//     }),
+//     glsl()
+//   ]
+// }));
 
 // app.use('/ivs2.js', function(request, response) {
 //   const make = spawn('make', ['build2/ivs.js']);
@@ -86,27 +89,27 @@ app.use('/ivs2.js', rollup.serve({
 //   });
 // });
 
-app.use('/ivs.js', function(request, response) {
-  const make = spawn('make');
-  make.stdout.on('data', d => {
-    console.log(d.toString());
-  });
-  make.stderr.on('data', d => {
-    console.log(d.toString());
-  });
-  make.on('close', (code) => {
-    console.log(`Done building with status code ${code}`);
-    const build_path = path.resolve(__dirname, 'build', 'ivs.js');
-    express.static(build_path).apply(express, arguments);
-  });
-});
+// app.use('/ivs.js', function(request, response) {
+//   const make = spawn('make');
+//   make.stdout.on('data', d => {
+//     console.log(d.toString());
+//   });
+//   make.stderr.on('data', d => {
+//     console.log(d.toString());
+//   });
+//   make.on('close', (code) => {
+//     console.log(`Done building with status code ${code}`);
+//     const build_path = path.resolve(__dirname, 'build', 'ivs.js');
+//     express.static(build_path).apply(express, arguments);
+//   });
+// });
 
 //
 // Static Files
 //
 
-var staticPath = path.resolve(__dirname, 'static');
-
-app.use('/', express.static(staticPath));
+// var staticPath = path.resolve(__dirname, 'static');
+//
+// app.use('/', express.static(staticPath));
 
 module.exports = app;
